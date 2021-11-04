2c455b55a5831e78c6b95df73bd1d5232e37d932
KenChen1034 <60757144+KenChen1034@users.noreply.github.com>
2021-11-03 23:37:55 -0700

    Commit1

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -25,6 +25,8 @@ const router = new Router(function () {
    * This will only be two single lines
    * If you did this right, you should see the recipe cards just like last lab
    */
+  document.querySelector('.section--recipe-cards').classList.add("shown");
+  document.querySelector('.section--recipe-expand').classList.remove("shown");
 });
 
 window.addEventListener('DOMContentLoaded', init);
@@ -55,6 +57,16 @@ function initializeServiceWorker() {
    *  TODO - Part 2 Step 1
    *  Initialize the service worker set up in sw.js
    */
+  if('serviceWorker' in navigator){
+    window.addEventListener('load', function(){
+      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err){
+        console.log('ServiceWorker registration failed:', err);
+      });
+    });
+  }
+  
 }
 
 /**
@@ -117,6 +129,23 @@ function createRecipeCards() {
    * all the recipes. (bonus - add the class 'hidden' to every recipe card with 
    * an index greater  than 2 in your for loop to make show more button functional)
    */
+  for(let i = 1; i < recipes.length; i++){
+    const newRecipeCard = document.createElement('recipe-card');
+    if(i >2){
+      newRecipeCard.classList.add('hidden');
+    }
+    newRecipeCard.data = recipeData[recipes[i]];
+    
+    const Newpage = recipeData[recipes[i]]['page-name'];
+    router.addPage(Newpage, function(){
+      document.querySelector('.section--recipe-cards').classList.remove('shown');
+      document.querySelector('.section--recipe-expand').classList.add('shown');
+      document.querySelector('recipe-expand').data = recipeData[recipes[i]];
+    });
+    bindRecipeCard(newRecipeCard, Newpage);
+
+    document.querySelector('.recipe-cards--wrapper').appendChild(newRecipeCard);
+  }
 }
 
 /**
@@ -172,6 +201,12 @@ function bindEscKey() {
    * if the escape key is pressed, use your router to navigate() to the 'home'
    * page. This will let us go back to the home page from the detailed page.
    */
+  document.addEventListener('keydown', function(event){
+    if(event.key == "Escape"){
+      router.navigate('home');
+    }
+  });
+
 }
 
 /**
@@ -193,4 +228,13 @@ function bindPopstate() {
    * so your navigate() function does not add your going back action to the history,
    * creating an infinite loop
    */
+  window.addEventListener('popstate', event =>{
+    if(event.state ==null){
+      router.navigate('home',true);
+    }
+    else{
+      router.navigate(event.state,true);
+    }
+  });
+  
 }
