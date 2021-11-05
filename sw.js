37d716fc58435f66ba8b00a28183e6a8262f992c
Ken Chen <60757144+KenChen1034@users.noreply.github.com>
2021-11-05 16:31:15 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -54,12 +54,13 @@ self.addEventListener('fetch', function (event) {
    */
   event.respondWith(
     caches.match(event.request)
-    .then(function(response){
-      if(response){
-        return response;
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
       }
-      return fetch(event.request);
-    }
     )
   );
 });
