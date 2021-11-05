37e602934c02a359efa793cb1ffde49eba9b4e48
Ken Chen <60757144+KenChen1034@users.noreply.github.com>
2021-11-05 16:24:54 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,6 @@
 
 const CACHE_NAME = 'lab-7-starter';
 const urlToCache = [
-  'kenchen1034.github.io/Lab7_Starter',
   'assets/components/RecipeCard.js',
   'assets/components/RecipeExpand.js',
   'assets/scripts/Router.js',
